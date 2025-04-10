6c2999d86d5542095aa9e8c4579f977c588d38b1
coderfee <coderfee@outlook.com>
2025-04-10 08:40:26 +0800

    refactor: remove axios-cache-interceptor and update stockClient export

--- lib/stockClient.ts
+++ lib/stockClient.ts
@@ -1,17 +1,14 @@
 import axios from 'axios'
-import { setupCache } from 'axios-cache-interceptor'
 import { Redis } from 'ioredis'
 
 const redis = new Redis()
 
-export const client = axios.create({
+export const stockClient = axios.create({
   baseURL: 'http://127.0.0.1:8080/api/public',
   headers: {
     'Content-Type': 'application/json',
   },
   timeout: 10000,
 })
-
-const stockClient = setupCache(client)
 
 export default stockClient
